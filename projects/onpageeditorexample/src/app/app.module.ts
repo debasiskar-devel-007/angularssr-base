9714988a722b1b5c8eea343eefc463abf43640b3
debasiskar-devel-007 <debasiskar007@gmail.com>
2019-07-29 18:31:13 +0530

    29th

--- projects/onpageeditorexample/src/app/app.module.ts
+++ projects/onpageeditorexample/src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import {OnPageEditorModule} from 'on-page-editor'
+import {OnPageEditorModule} from 'on-page-editor';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutusComponent,
+    ContactusComponent
   ],
   imports: [
-    BrowserModule,OnPageEditorModule
+    BrowserModule,OnPageEditorModule,AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
